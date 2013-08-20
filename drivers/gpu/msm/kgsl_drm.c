8e88f416399ead3c308228205ed77fda870ec060
Eunchul Kim <chulspro.kim@samsung.com>
2013-08-20 13:20:25 +0900

    kgsl/drm: add fake vsync for WB operation
    We need to support fake vsync for UI updating in the WB operation with
    LCD off statement.
    
    Change-Id: I564b979e51c8ee6dd6ad377e4d698a271f90780d
    Signed-off-by: Eunchul Kim <chulspro.kim@samsung.com>
    Signed-off-by: Mathew Karimpanal <kjmathew@codeaurora.org>

--- drivers/gpu/msm/kgsl_drm.c
+++ drivers/gpu/msm/kgsl_drm.c
@@ -90,6 +90,7 @@ struct drm_kgsl_private {
 	void __iomem *regs;
 	size_t reg_size;
 	unsigned int irq;
+	struct work_struct fake_vbl_work;
 	atomic_t vbl_received[DRM_KGSL_CRTC_MAX];
 };
 
@@ -1307,11 +1308,22 @@ kgsl_drm_get_vblank_counter(struct drm_device *dev, int crtc)
 static int
 kgsl_drm_enable_vblank(struct drm_device *dev, int crtc)
 {
+	struct drm_kgsl_private *dev_priv =
+		(struct drm_kgsl_private *)dev->dev_private;
+
 	DRM_DEBUG("%s:crtc[%d]\n", __func__, crtc);
 
 	if (crtc > DRM_KGSL_CRTC_MAX) {
 		DRM_ERROR("failed to disable vblank.\n");
 		return -EINVAL;
+	}
+
+	switch (crtc) {
+	case DRM_KGSL_CRTC_FAKE:
+		schedule_work(&dev_priv->fake_vbl_work);
+		break;
+	default:
+		break;
 	}
 
 	return 0;
@@ -1363,6 +1375,20 @@ kgsl_drm_irq_handler(DRM_IRQ_ARGS)
 
 irq_done:
 	return IRQ_HANDLED;
+}
+
+static void
+kgsl_drm_fake_vblank_handler(struct work_struct *work)
+{
+	struct drm_kgsl_private *dev_priv = container_of(work,
+			struct drm_kgsl_private, fake_vbl_work);
+
+	/* refresh rate is about 60Hz. */
+	usleep_range(15000, 16000);
+
+	DRM_DEBUG("%s\n", __func__);
+	drm_handle_vblank(dev_priv->drm_dev, DRM_KGSL_CRTC_FAKE);
+
 }
 
 static void
@@ -1654,6 +1680,9 @@ static int kgsl_drm_load(struct drm_device *dev, unsigned long flags)
 	/* acquire interrupt */
 	dev_priv->irq = platform_get_irq_byname(pdev, KGSL_DRM_IRQ);
 
+	/* init workqueue for fake vsync */
+	INIT_WORK(&dev_priv->fake_vbl_work, kgsl_drm_fake_vblank_handler);
+
 	/* store dev structure */
 	dev_priv->drm_dev = dev;
 
@@ -1669,6 +1698,7 @@ static int kgsl_drm_load(struct drm_device *dev, unsigned long flags)
 	 * 2 : secondary device(HDMI)
 	 * 3 : writeback 0 device(Rotator)
 	 * 4 : writeback 2 device(WFD)
+	 * 5 : fake vblank device(Standby mode)
 	 */
 	ret = drm_vblank_init(dev, DRM_KGSL_CRTC_MAX);
 	if (ret) {
