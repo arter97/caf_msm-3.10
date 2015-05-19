558cfcbf7b1fdb4097e1eae64769fca876f34aee
AJ Lindell <alindell@codeaurora.org>
2015-05-19 12:53:39 -0600

    msm: msm_bus: Introduce bus rule update API
    Adding the bus_rule_update API to allow clients to
    update existing rules with a single function call
    as well as an API to immediately evaluate the current
    bus rules.
    
    Change-Id: Ia86e5a9c82e7f21c6445c4c56fe0197fc11e6615
    Signed-off-by: AJ Lindell <alindell@codeaurora.org>

--- include/linux/msm_bus_rules.h
+++ include/linux/msm_bus_rules.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2014, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2014-2015, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -54,6 +54,10 @@ void msm_rule_register(int num_rules, struct bus_rule_type *rule,
 				struct notifier_block *nb);
 void msm_rule_unregister(int num_rules, struct bus_rule_type *rule,
 						struct notifier_block *nb);
+bool msm_rule_update(struct bus_rule_type *old_rule,
+				struct bus_rule_type *new_rule,
+				struct notifier_block *nb);
+void msm_rule_evaluate_rules(int node);
 void print_rules_buf(char *buf, int count);
 bool msm_rule_are_rules_registered(void);
 #else
@@ -72,6 +76,15 @@ static inline void print_rules_buf(char *buf, int count)
 static inline bool msm_rule_are_rules_registered(void)
 {
 	return false;
+}
+static inline bool msm_rules_update(struct bus_rule_type *old_rule,
+					struct bus_rule_type *new_rule,
+					struct notifier_block *nb)
+{
+	return false;
+}
+static inline void msm_rule_evaluate_rules(int node)
+{
 }
 #endif /* defined(CONFIG_BUS_TOPOLOGY_ADHOC) */
 #endif /* _ARCH_ARM_MACH_MSM_BUS_RULES_H */
