abb63108d76e871879569c7427d0e47aa6ec328a
Phani Kumar Uppalapati <phaniu@codeaurora.org>
2015-06-03 01:36:55 -0700

    ASoC: wcd9335: Add support to expose wcd9335 codec version
    Create wcd9335 module and version info entry to expose
    the chipset ID and version information of wcd9335 audio
    codec. Clients can use this information to identify codec
    and its version which in turn can be used to access its
    registers.
    
    Change-Id: I9d6732383ff2ac047e707c3d27234df5ded33078
    Signed-off-by: Phani Kumar Uppalapati <phaniu@codeaurora.org>

--- sound/soc/codecs/wcd9335.h
+++ sound/soc/codecs/wcd9335.h
@@ -115,4 +115,6 @@ extern void tasha_event_register(
 				enum wcd9335_codec_event),
 	struct snd_soc_codec *codec);
 extern enum codec_variant tasha_codec_ver(void);
+extern int tasha_codec_info_create_codec_entry(struct snd_info_entry *,
+					       struct snd_soc_codec *);
 #endif
