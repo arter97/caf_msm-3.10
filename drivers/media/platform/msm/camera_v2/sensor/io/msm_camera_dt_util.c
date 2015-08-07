34b4a9793dd4c21e4031799407181b76c648e89d
Todor Tomov <ttomov@codeaurora.org>
2015-08-06 18:31:32 -0700

    msm: camera: Export IR LED device to mm-qcamera-daemon
    Extend sensor_info structure with one more field to be able to export the
    LED IR device to mm-qcamera-daemon.
    
    To point the LED IR device in the dtsi file, add configuration
    like the following inside the relevant "camera" section:
    
    qcom,camera@0 {
      ...
      qcom,ir-led-src = <&ir_led0>;
      ...
    };
    
    Change-Id: If95b11e9056fca1f944d59569130d3127e02eeef
    Signed-off-by: Ivan Evlogiev <ivanevlogiev@codeaurora.org>

--- drivers/media/platform/msm/camera_v2/sensor/io/msm_camera_dt_util.c
+++ drivers/media/platform/msm/camera_v2/sensor/io/msm_camera_dt_util.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2013-2015, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2013-2016, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -280,6 +280,22 @@ int msm_sensor_get_sub_module_index(struct device_node *of_node,
 			goto ERROR;
 		}
 		sensor_info->subdev_id[SUB_MODULE_LED_FLASH] = val;
+		of_node_put(src_node);
+		src_node = NULL;
+	}
+
+	src_node = of_parse_phandle(of_node, "qcom,ir-led-src", 0);
+	if (!src_node) {
+		CDBG("%s:%d src_node NULL\n", __func__, __LINE__);
+	} else {
+		rc = of_property_read_u32(src_node, "cell-index", &val);
+		CDBG("%s qcom,ir led cell index %d, rc %d\n", __func__,
+			val, rc);
+		if (rc < 0) {
+			pr_err("%s:%d failed %d\n", __func__, __LINE__, rc);
+			goto ERROR;
+		}
+		sensor_info->subdev_id[SUB_MODULE_IR_LED] = val;
 		of_node_put(src_node);
 		src_node = NULL;
 	}
