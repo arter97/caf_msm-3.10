666d18380625ffde73bdb1aecbe18241c39e17c1
Kranthikumar Kurapati <kkurap@codeaurora.org>
2015-08-15 22:19:41 -0400

    msm: fsm9900: Fix kref on emac devicetree node
    emac_dt_update() routine that updates emac device tree nodes with the
    ethernet MAC address is returning to its caller without decrementing
    kref count on the corresponding emac device tree node. This change
    fixes this by incrementing the reference count by calling the
    function of_node_put() before returning.
    
    Change-Id: Ib732370d15b34cc8b41681bff040a5608a684db8
    Acked-by: Jinesh Kunnathully Jayakumar <jineshk@qti.qualcomm.com>
    Signed-off-by: Kranthikumar Kurapati <kkurap@codeaurora.org>

--- arch/arm/mach-msm/board-fsm9900.c
+++ arch/arm/mach-msm/board-fsm9900.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2013-2014, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2013-2015, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -294,6 +294,8 @@ static int emac_dt_update(int cell, phys_addr_t addr, unsigned long size)
 		of_update_property(np, pmac);
 	else
 		of_add_property(np, pmac);
+
+	of_node_put(np);
 
 out:
 	if (retval && pmac)
