8f8a170fc7f487c34750cdcf9bfbfdc2999448ab
Manaf Meethalavalappu Pallikunhi <manafm@codeaurora.org>
2015-09-18 20:54:14 +0530

    msm: thermal: Add ftrace to KTM progressive mitigation
    Add ftrace events for KTM progressive algorithm mitigation.
    Below ftrace events are added to KTM,
    thermal_progressive_sampling
    thermal_progressive_state
    thermal_progressive_mitigate
    
    Change-Id: Iec8a3a62f5655a59901ebaf744233457c22c84a5
    Signed-off-by: Manaf Meethalavalappu Pallikunhi <manafm@codeaurora.org>

--- drivers/thermal/msm_thermal.c
+++ drivers/thermal/msm_thermal.c
@@ -71,6 +71,8 @@
 #define MSM_THERMAL_THRESH_CLR    "thresh_clr_degc"
 #define MSM_THERMAL_THRESH_UPDATE "update"
 #define MAX_CPU_NAME 10
+#define OVERALL_TYPE "Overall"
+#define INDIVIDUAL_TYPE  "Individual"
 
 #define THERM_CREATE_DEBUGFS_DIR(_node, _name, _parent, _ret) \
 	do { \
@@ -3290,6 +3292,9 @@ static void do_prog_freq_control(struct thermal_progressive_rule *prog,
 		temp, ret);
 		return;
 	}
+	trace_thermal_progressive_mitigate(prog->sensor_info->name,
+		cluster_ptr ? cluster_ptr->cluster_id : 0,
+		freq_table[idx].frequency);
 	pr_debug("Sensor:%s Limiting Cluster%d max frequency to %u. Temp:%ld\n",
 		prog->sensor_info->name, cluster_ptr->cluster_id,
 		freq_table[idx].frequency, temp);
@@ -3965,15 +3970,33 @@ static void msm_prog_freq_notify(struct therm_threshold *trig_thresh)
 	switch (prog->overall_prog_state) {
 	case MSM_THERM_PROGRESSIVE_SAMPLING:
 		do_prog_freq_control(prog, prog_thresh->curr_max_temp);
-		if (prog->cur_freq_idx == prog->freq_idx_high)
+		if (prog->cur_freq_idx == prog->freq_idx_high) {
 			prog->overall_prog_state =
 				MSM_THERM_PROGRESSIVE_PAUSED;
+			trace_thermal_progressive_state(
+				prog->sensor_info->name, OVERALL_TYPE,
+				prog->overall_prog_state);
+			pr_debug("sensor=%s type=%s curr_state=%s\n",
+				prog->sensor_info->name, OVERALL_TYPE,
+				prog->overall_prog_state ?
+				(prog->overall_prog_state == 1 ?
+				"paused" : "monitor") : "sampling");
+		}
 		break;
 	case MSM_THERM_PROGRESSIVE_MONITOR:
 		do_prog_freq_control(prog, prog_thresh->curr_max_temp);
-		if (prog->cur_freq_idx != prog->freq_idx_high)
+		if (prog->cur_freq_idx != prog->freq_idx_high) {
 			prog->overall_prog_state =
 				MSM_THERM_PROGRESSIVE_SAMPLING;
+			trace_thermal_progressive_state(
+				prog->sensor_info->name, OVERALL_TYPE,
+				prog->overall_prog_state);
+			pr_debug("sensor=%s type=%s curr_state=%s\n",
+				prog->sensor_info->name, OVERALL_TYPE,
+				prog->overall_prog_state ?
+				(prog->overall_prog_state == 1 ?
+				"paused" : "monitor") : "sampling");
+		}
 		break;
 	case MSM_THERM_PROGRESSIVE_PAUSED:
 		break;
@@ -4136,6 +4159,9 @@ static void msm_progressive_monitor(struct work_struct *work)
 		pr_debug("Sensor:%d temp:%ld polling_delay_ms:%d\n",
 			sensor_data->sensor_id, temp, prog->polling_delay_ms);
 
+		trace_thermal_progressive_sampling(prog->sensor_info->name,
+			temp);
+
 		switch (sensor_data->prog_state) {
 		case MSM_THERM_PROGRESSIVE_SAMPLING:
 		case MSM_THERM_PROGRESSIVE_PAUSED:
@@ -4166,6 +4192,9 @@ static void msm_progressive_monitor(struct work_struct *work)
 				sensor_data->sensor_id);
 			break;
 		}
+		trace_thermal_progressive_state(
+			prog->sensor_info->name, INDIVIDUAL_TYPE,
+				sensor_data->prog_state);
 
 		if (sensor_data->prog_trip_clear) {
 			sensor_mgr_set_threshold(sensor_data->sensor_id,
@@ -4185,6 +4214,12 @@ static void msm_progressive_monitor(struct work_struct *work)
 		new_prog_state = MSM_THERM_PROGRESSIVE_PAUSED;
 
 start_sampling:
+	trace_thermal_progressive_state(
+		prog->sensor_info->name, OVERALL_TYPE, new_prog_state);
+	pr_debug("sensor=%s type=%s curr_state=%s\n",
+		prog->sensor_info->name, OVERALL_TYPE, new_prog_state ?
+		(new_prog_state == 1 ? "paused" : "monitor") : "sampling");
+
 	prog->overall_prog_state = new_prog_state;
 	prog_thresh->thresh_list[0].notify(&prog_thresh->thresh_list[0]);
 
