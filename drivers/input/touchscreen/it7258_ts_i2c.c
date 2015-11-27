e3ef538b1e71bc3f143af2127f4728307ab558ed
Shantanu Jain <shjain@codeaurora.org>
2015-11-27 12:34:38 +0530

    input: touchscreen: reduce lpm current of avdd reg for ITE tech
    Reduce lpm current of avdd regulator for ITE tech touch controller
    to save power dissipation in lpm state.
    
    Change-Id: I4b0c532096f1555561ad33e937cb947e85f987cd
    Signed-off-by: Shantanu Jain <shjain@codeaurora.org>

--- drivers/input/touchscreen/it7258_ts_i2c.c
+++ drivers/input/touchscreen/it7258_ts_i2c.c
@@ -169,6 +169,7 @@ struct IT7260_ts_platform_data {
 	unsigned int disp_maxy;
 	unsigned num_of_fingers;
 	unsigned int reset_delay;
+	unsigned int avdd_lpm_cur;
 	bool low_reset;
 };
 
@@ -1491,6 +1492,14 @@ static int IT7260_parse_dt(struct device *dev,
 		return rc;
 	}
 
+	rc = of_property_read_u32(np, "ite,avdd-lpm-cur", &temp_val);
+	if (!rc) {
+		pdata->avdd_lpm_cur = temp_val;
+	} else if (rc && (rc != -EINVAL)) {
+		dev_err(dev, "Unable to read avdd lpm current value %d\n", rc);
+		return rc;
+	}
+
 	pdata->low_reset = of_property_read_bool(np, "ite,low-reset");
 
 	rc = IT7260_get_dt_coords(dev, "ite,display-coords", pdata);
@@ -1873,6 +1882,15 @@ static int IT7260_ts_resume(struct device *dev)
 
 	if (device_may_wakeup(dev)) {
 		if (gl_ts->device_needs_wakeup) {
+			/* Set active current for the avdd regulator */
+			if (gl_ts->pdata->avdd_lpm_cur) {
+				retval = reg_set_optimum_mode_check(gl_ts->avdd,
+						IT_I2C_ACTIVE_LOAD_UA);
+				if (retval < 0)
+					dev_err(dev, "Regulator avdd set_opt failed at resume rc=%d\n",
+					retval);
+			}
+
 			gl_ts->device_needs_wakeup = false;
 			disable_irq_wake(gl_ts->client->irq);
 		}
@@ -1910,6 +1928,15 @@ static int IT7260_ts_suspend(struct device *dev)
 		if (!gl_ts->device_needs_wakeup) {
 			/* put the device in low power idle mode */
 			IT7260_ts_chipLowPowerMode(PWR_CTL_LOW_POWER_MODE);
+
+			/* Set lpm current for avdd regulator */
+			if (gl_ts->pdata->avdd_lpm_cur) {
+				retval = reg_set_optimum_mode_check(gl_ts->avdd,
+						gl_ts->pdata->avdd_lpm_cur);
+				if (retval < 0)
+					dev_err(dev, "Regulator avdd set_opt failed at suspend rc=%d\n",
+						retval);
+			}
 
 			gl_ts->device_needs_wakeup = true;
 			enable_irq_wake(gl_ts->client->irq);
