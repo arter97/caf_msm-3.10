9b5f74342e5acca9becfebb7aa16e13e3c9896ac
Karthik Reddy Katta <a_katta@codeaurora.org>
2016-04-20 16:40:42 +0530

    ASoC: msm: qdsp6v2: Fix improper assignment of mem_state
    Due to propagation error mem_state is incorrectly
    assigned to "-1". Correct the propagation error.
    
    Change-Id: I45cac128f74ab267e43d5caa2107ca0fd34de83c
    Signed-off-by: Karthik Reddy Katta <a_katta@codeaurora.org>
    Signed-off-by: Dhananjay Kumar <dhakumar@codeaurora.org>

--- sound/soc/msm/qdsp6v2/q6asm.c
+++ sound/soc/msm/qdsp6v2/q6asm.c
@@ -608,7 +608,7 @@ int send_asm_custom_topology(struct audio_client *ac)
 
 	remap_cal_data(cal_block);
 	q6asm_add_hdr_custom_topology(ac, &asm_top.hdr, sizeof(asm_top), TRUE);
-	atomic_set(&ac->mem_state, -1);
+	atomic_set(&ac->mem_state, 1);
 	asm_top.hdr.opcode = ASM_CMD_ADD_TOPOLOGIES;
 	asm_top.payload_addr_lsw = lower_32_bits(cal_block->cal_data.paddr);
 	asm_top.payload_addr_msw = populate_upper_32_bits(
